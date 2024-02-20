0546249808d59cfe44702f1f28c7c864ed2b23f2
gurung88k <156258318+gurung88k@users.noreply.github.com>
2024-02-20 10:21:40 -0500

    foods.php

--- MidtermTest/foods.php
+++ MidtermTest/foods.php
@@ -1,24 +1,31 @@
 <html>
 <body>
+
 <?php
 // Include necessary files
 include('SHARED/db.php');
-
+include('select-cuisine.php');
 // Capture form inputs into variables
-$cuisinename = $_POST['cuisine'];
+$cuisineName = $_POST['cuisine'];
 
 include('shared/db.php');
 
-$sql= "SELECT * FROM foods WHERE cuisine= :cuisinename";
+$sql= "SELECT * FROM foods WHERE foods['cuisine']=:cuisineName";
 $cmd = $db->prepare($sql);
-$cmd->bindParam(':cuisinename', $cuisinename, PDO::PARAM_STR);
+$cmd->bindParam(':cuisineName', $cuisineName, PDO::PARAM_STR);
 $cmd->execute();
-$show = $cmd->fetchAll();
+$foods = $cmd->fetchAll();
+// to display
+foreach ($foods as $food) {
+                if ($food['cuisine'] == $cuisineName) {
+                    <a href="delete-food.php?showId=' .$food['Id'] . '" onclick="return confirmDelete();">echo $foods['name']</a>
 
+                }
+}
 $db= null;
 ?>
-foreach ($cuisines as $cuisine):
 
-<a href="delete-food.php?foodId= ' .$food['foodId'] .'"onclick=return confirmDelete();">echo $cuisine['name']</a>
+//for show ID
 
+</body>
 </html>
